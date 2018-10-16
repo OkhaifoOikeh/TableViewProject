aca6e258182d013e8fc1f698bdfbf7788c556048
OkhaifoOikeh <okhaifooikeh@gmail.com>
2018-10-16 00:37:12 -0400

    Initial Commit

---
+++ TableViewProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewProject
+//
+//  Created by Okhaifo Oikeh on 10/16/18.
+//  Copyright © 2018 Okhaifo Oikeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
